3c86fec30bb7786c00a1ca5f20d05d2d4b21024b
Brandon F Vargas <brandondinog99@gmail.com>
2026-06-08 22:28:30 -0600

    API's cambiadas

--- config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-GOOGLE_API_KEY = "AQ.Ab8RN6JbzqNtRzQMdn3a8yQi4Le6BBtQrnK07gLVEc0ZSEbqZg"
-GEMINI_MODEL = "gemini-2.5-flash"
-
-# config.py
-OPENROUTER_API_KEY = "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"
-OPENROUTER_URL = "https://openrouter.ai/api/v1/chat/completions"
-OR_MODEL = "google/gemini-3-flash-preview"  # o "google/gemini-2.0-flash-exp" según disponibilidad
